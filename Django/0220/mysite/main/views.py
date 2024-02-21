10e03f3fb7de4c535c89b8824e42988a8fa893b2
진성현 <140477594+Jinseonghyun@users.noreply.github.com>
2024-02-21 09:03:33 +0900

    Django

---
+++ Django/0220/mysite/main/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "main/index.html")
+
+def a(request):
+    return render(request, "main/a.html")
+
+def b(request):
+    return render(request, "main/b.html")
+
+def c(request):
+    return render(request, "main/c.html")
+
+def seonghyeon(request):
+    return render(request, "main/seonghyeon.html")
+
+def orm(request):
+    return render(request, "main/orm.html")
